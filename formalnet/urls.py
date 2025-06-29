0caee750c3db337c8d422125112abe4f1a5b5865
Bigyan <bigyan8aryal@gmail.com>
2025-06-29 18:03:04 +0545

    authentication:login,logout,register

--- formalnet/urls.py
+++ formalnet/urls.py
@@ -1,11 +1,19 @@
 from . import views
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.index,name='index'),
     path('contact/',views.contact,name='contact'),
 
-    # path('login/',views.loginPage,name='login'),
-    # path('logout/',views.logoutUser,name='logout'),
-    # path('register/',views.registerPage,name='register'),
+    path('register/',views.register_page,name='register'),
+    path('login/',views.login_view,name='login'),
+    path('logout/',views.logoutUser,name='logout'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- formalnet/views.py
+++ formalnet/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout 
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def index(request):
@@ -9,3 +13,32 @@ def index(request):
 def contact(request):
     return HttpResponse("This is contact.")
 
+def register_page(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'formalnet/register_done.html', {'new_user': new_user})
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'formalnet/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    form = AuthenticationForm(request, data=request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        return redirect('index')
+
+    return render(request, 'formalnet/login.html', {'form': form})
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
