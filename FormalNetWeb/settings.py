72108cba9462e3e087e74c7390c4cf10f3d62ff0
Bigyan <bigyan8aryal@gmail.com>
2025-07-15 23:43:42 +0545

    ui changes

--- FormalNetWeb/settings.py
+++ FormalNetWeb/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'formalnet.apps.FormalnetConfig'
+    'formalnet.apps.FormalnetConfig',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
